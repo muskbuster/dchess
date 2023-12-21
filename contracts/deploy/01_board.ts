42e7ea55c5690404bf212a0cfdd4e94e29ea75e4
Raunak <60561127+RnkSngh@users.noreply.github.com>
2023-12-21 16:35:44 -0500

    add linting + formatter

--- contracts/deploy/01_board.ts
+++ contracts/deploy/01_board.ts
@@ -1,21 +1,21 @@
-import { HardhatRuntimeEnvironment } from "hardhat/types";
-import { DeployFunction } from "hardhat-deploy/types";
+import { HardhatRuntimeEnvironment } from "hardhat/types"
+import { DeployFunction } from "hardhat-deploy/types"
 
 const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
-  const { deployments, getNamedAccounts } = hre;
-  const [deployer] = await hre.getUnnamedAccounts();
-  console.log("deployer", deployer);
+	const { deployments } = hre
+	const [deployer] = await hre.getUnnamedAccounts()
+	console.log("deployer", deployer)
 
-  await deployments.deploy("Board", {
-    from: deployer,
-    args: [deployer],
-  });
+	await deployments.deploy("Board", {
+		from: deployer,
+		args: [deployer],
+	})
 
-  if (hre.network.name != "hardhat") {
-    const board = await deployments.get("Board");
-    console.log(`Board deployed to ${board.address} with owner as ${deployer}`);
-  }
-};
+	if (hre.network.name != "hardhat") {
+		const board = await deployments.get("Board")
+		console.log(`Board deployed to ${board.address} with owner as ${deployer}`)
+	}
+}
 
-func.tags = ["Board"];
-export default func;
+func.tags = ["Board"]
+export default func
--- contracts/hardhat.config.ts
+++ contracts/hardhat.config.ts
@@ -1,45 +1,45 @@
-import { HardhatUserConfig } from "hardhat/config";
-import "@nomicfoundation/hardhat-toolbox";
-import "hardhat-deploy";
+import { HardhatUserConfig } from "hardhat/config"
+import "@nomicfoundation/hardhat-toolbox"
+import "hardhat-deploy"
 
-const PRIVATE_KEY = process.env.PRIVATE_KEY! as string;
+const PRIVATE_KEY = process.env.PRIVATE_KEY! as string
 const ALCHEMY_BASE_GOERLI_HTTPS = process.env.ALCHEMY_BASE_GOERLI_HTTPS
-  ? process.env.ALCHEMY_BASE_GOERLI_HTTPS
-  : "https://goerli.base.org";
-const ALCHEMY_BASE_HTTPS = process.env.ALCHEMY_BASE_HTTPS as string;
-const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY as string;
+	? process.env.ALCHEMY_BASE_GOERLI_HTTPS
+	: "https://goerli.base.org"
+// const ALCHEMY_BASE_HTTPS = process.env.ALCHEMY_BASE_HTTPS as string
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY as string
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.20",
-  networks: {
-    // for mainnet
-    "base-mainnet": {
-      url: ALCHEMY_BASE_GOERLI_HTTPS,
-      accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : "remote",
-      gasPrice: 1000000000,
-    },
-    // for testnet
-    "base-goerli": {
-      url: ALCHEMY_BASE_GOERLI_HTTPS,
-      accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : "remote",
-      gasPrice: 1000000000,
-    },
-  },
-  etherscan: {
-    apiKey: {
-      "base-goerli": ETHERSCAN_API_KEY,
-    },
-    customChains: [
-      {
-        network: "base-goerli",
-        chainId: 84531,
-        urls: {
-          apiURL: "https://api-goerli.basescan.org/api",
-          browserURL: "https://goerli.basescan.org",
-        },
-      },
-    ],
-  },
-};
+	solidity: "0.8.20",
+	networks: {
+		// for mainnet
+		"base-mainnet": {
+			url: ALCHEMY_BASE_GOERLI_HTTPS,
+			accounts: PRIVATE_KEY ? [PRIVATE_KEY] : "remote",
+			gasPrice: 1000000000,
+		},
+		// for testnet
+		"base-goerli": {
+			url: ALCHEMY_BASE_GOERLI_HTTPS,
+			accounts: PRIVATE_KEY ? [PRIVATE_KEY] : "remote",
+			gasPrice: 1000000000,
+		},
+	},
+	etherscan: {
+		apiKey: {
+			"base-goerli": ETHERSCAN_API_KEY,
+		},
+		customChains: [
+			{
+				network: "base-goerli",
+				chainId: 84531,
+				urls: {
+					apiURL: "https://api-goerli.basescan.org/api",
+					browserURL: "https://goerli.basescan.org",
+				},
+			},
+		],
+	},
+}
 
-export default config;
+export default config
