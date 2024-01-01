8b29ef9ad41adb006e7fcfc0821cbde932250ee7
Uneeb Agha <uneeb.agha@gmail.com>
2024-01-01 12:42:41 -0500

    chess component

--- base-chess/components/Screen/Create/CreateScreen.tsx
+++ base-chess/components/Screen/Create/CreateScreen.tsx
@@ -1,5 +1,19 @@
+// @ts-ignore
+import { NextEditor } from "next-chessground";
+import { useRef } from "react";
+
 const CreateScreen = () => {
-  return <div>Create</div>;
+  const ref = useRef();
+
+  const handleSelect = (fen: any) => {
+    //
+  };
+
+  return (
+    <div className="h-96 w-96">
+      <NextEditor ref={ref} onSelect={handleSelect} />
+    </div>
+  );
 };
 
 export default CreateScreen;
--- base-chess/components/Screen/Play/PlayScreen.tsx
+++ base-chess/components/Screen/Play/PlayScreen.tsx
@@ -1,5 +1,15 @@
+// @ts-ignore
+import { NextChessground } from "next-chessground";
+
+// Documentation for the NextChessground
+// https://github.com/victorocna/next-chessground/blob/master/lib/Chessground.jsx
+
 const PlayScreen = () => {
-  return <div>Play</div>;
+  return (
+    <div className="h-96 w-96">
+      <NextChessground fen="8/1kPK4/8/8/8/8/8/8 w - - 0 1" />
+    </div>
+  );
 };
 
 export default PlayScreen;
