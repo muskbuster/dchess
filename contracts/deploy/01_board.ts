f90214fd784df89999e68bb45ec7777ae5282b6e
Raunak <60561127+RnkSngh@users.noreply.github.com>
2024-01-03 18:41:53 -0500

    add deploy scripts for dev setup

--- contracts/deploy/01_board.ts
+++ contracts/deploy/01_board.ts
@@ -4,7 +4,6 @@ import { DeployFunction } from "hardhat-deploy/types"
 const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
 	const { deployments } = hre
 	const [deployer] = await hre.getUnnamedAccounts()
-	console.log("deployer", deployer)
 
 	await deployments.deploy("Board", {
 		from: deployer,
