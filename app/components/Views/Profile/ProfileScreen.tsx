80dfc476572d85deb921438aa01e502151179808
tarunkumar2112 <tarunkumarz211286@gmail.com>
2024-02-23 17:07:56 +0530

    Updated accout page on mobile

--- app/components/Views/Profile/ProfileScreen.tsx
+++ app/components/Views/Profile/ProfileScreen.tsx
@@ -14,9 +14,9 @@ const ProfileStats = ({
 }) => {
   return (
     <div>
-      <div>Rating: {profile.ratings}</div>
-      <div>Solved: {profile.totalSolved}</div>
-      <div>Attempted: {profile.totalAttempted}</div>
+      <div className="md:block flex flex-row justify-between"><div>Rating:</div> <div>{profile.ratings}</div></div>
+      <div className="md:block flex flex-row justify-between"><div>Solved:</div> <div>{profile.totalSolved}</div></div>
+      <div className="md:block flex flex-row justify-between"><div>Attempted:</div> <div>{profile.totalAttempted}</div></div>
     </div>
   );
 };
@@ -31,13 +31,13 @@ const ProfileScreen = ({
   const { profile } = useFetchProfile(address);
 
   return (
-    <div className="my-flex md:flex-row my-flex-col w-full justify-center">
+    <div className="my-flex md:flex-row my-flex-col w-full justify-center ">
       <div className="my-flex flex-col space-y-5 md:w-1/4 my-w-full">
-        <div className="text-2xl mb-5"> Account</div>
+        <div className="md:text-2xl text-lg mb-5"> Account</div>
         <ProfileStats profile={profile} />
       </div>
-      <div className="my-flex flex-col space-y-5 md:w-1/2 my-w-full">
-        <div className="text-2xl mb-5"> NFTs Owned</div>
+      <div className="my-flex flex-col space-y-5 md:w-1/2 my-w-full md:mt-0 mt-[20px]">
+        <div className="md:text-2xl text-lg mb-5"> NFTs Owned</div>
         <Gallery ids={profile.nftsOwned} />
       </div>
     </div>
