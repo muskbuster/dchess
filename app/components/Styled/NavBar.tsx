91c34b971a3f7ac837ba85d38dd358d9fc07c54e
tarunkumar2112 <tarunkumarz211286@gmail.com>
2024-03-01 10:38:52 +0530

    updated the NavBar tsx file

--- app/components/Styled/NavBar.tsx
+++ app/components/Styled/NavBar.tsx
@@ -5,6 +5,7 @@ import useAccessControl from "@/hooks/useAccessControl";
 import { useUserInfo } from "@/contexts/UserInfoContext";
 import { zeroAddress } from "viem";
 import { useRouter } from "next/navigation";
+import NavToggle from './NavToggle';
 
 const NavBar = ({ loggedIn = false }: { loggedIn: boolean }) => {
   const { login } = usePrivy();
@@ -15,10 +16,13 @@ const NavBar = ({ loggedIn = false }: { loggedIn: boolean }) => {
   const router = useRouter();
 
   return (
-    <nav className="navbar bg-slate-600 fixed w-full z-20 top-0 start-0">
-      <div className="flex flex-row justify-between w-full">
-        <div className="flex flex-row space-x-2">
-          <StyledButton wide={false} onClick={() => router.push("/play/0")}>
+    <nav className="navbar bg-slate-600 fixed w-full z-20 top-0 start-0 border-b-[1px] border-solid border-white md:border-b-0">
+      <div className="flex flex-row justify-between w-full sm:static relative">
+      <div className="block md:hidden">
+          <NavToggle toggleNav={toggleNav} isOpen={undefined} />
+        </div>
+        <div className="{`md:flex ${isNavOpen ? 'flex-row' : 'hidden'} gap-3 sm:gap-0 sm:space-x-2 space-x-0 sm:pl-0 sm:pr-0 pl-[10px] pr-[10px] sm:static absolute left-0 right-0 top-[100%] bg-slate-600`}">
+          <StyledButton className='sm:w-fit my-w-full' wide={false} onClick={() => router.push("/play/0")}>
             Play
           </StyledButton>
           <div
@@ -33,7 +37,7 @@ const NavBar = ({ loggedIn = false }: { loggedIn: boolean }) => {
                 : ""
             }`}
           >
-            <StyledButton
+            <StyledButton className='sm:w-fit my-w-full'
               wide={false}
               disabled={!loggedIn || !getOnLeaderboard}
               onClick={() => router.push("/stats")}
@@ -53,7 +57,7 @@ const NavBar = ({ loggedIn = false }: { loggedIn: boolean }) => {
                 : ""
             }`}
           >
-            <StyledButton
+            <StyledButton className='sm:w-fit my-w-full'
               wide={false}
               disabled={!loggedIn || !puzzleCreate}
               onClick={() => router.push("/create")}
