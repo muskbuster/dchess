052e9353fb76c5f0f0b842cd2347a1b27645b40d
tarunkumar2112 <tarunkumarz211286@gmail.com>
2024-03-02 10:35:37 +0530

    Updated the NavBar tsx file and added the NavToggle tsx file

--- app/components/Styled/NavBar.tsx
+++ app/components/Styled/NavBar.tsx
@@ -20,8 +20,10 @@ const NavBar = ({ loggedIn = false }: { loggedIn: boolean }) => {
   return (
     <nav className="navbar bg-slate-600 fixed w-full z-20 top-0 start-0 border-b-[1px] border-solid border-white md:border-b-0">
       <div className="flex flex-row justify-between w-full sm:static relative">
-        
-        <div className="flex flex-row space-x-2">
+      <div className="block md:hidden">
+          <NavToggle toggleNav={toggleNav} isOpen={undefined} />
+        </div>
+        <div className="{`md:flex ${isNavOpen ? 'flex-row' : 'hidden'} gap-3 sm:gap-0 sm:space-x-2 space-x-0 sm:pl-0 sm:pr-0 pl-[10px] pr-[10px] sm:static absolute left-0 right-0 top-[100%] bg-slate-600`}">
           <StyledButton wide={false} onClick={() => router.push("/play/0")}>
             Play
           </StyledButton>
