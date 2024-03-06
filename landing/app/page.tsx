f0a3d2a379ebfc819cd58dc5ad9fbb2fed6d16bd
nouraizasad <nouraizasad@hotmail.com>
2024-03-06 19:35:58 +0500

    landing page design updated

--- landing/app/page.tsx
+++ landing/app/page.tsx
@@ -1,25 +1,57 @@
-import Image from "next/image";
-import logo from "@/public/logo.png";
 import { FAQ } from "@/components/FAQ";
 
 export default function Home() {
+  const q3 = `How does Virtuoso Club work?`;
+  const a3_1 = `The protocol is designed such that it incentivizes “good” action that benefit the game of Chess. It introduces a new layer of governance.`;
+  const a3_2 = `As of today, Virtuoso Club's serves as a training platform to learn the game of chess:`;
+  const cards = [
+    {
+      title: 'a3_2_1',
+      desc: `Players can solve puzzles build their chess ratings`,
+      className: 'bg-pink-200 rounded-[16px] w-1/3 p-12 flex items-center justify-center bg-gradient-to-r from-[#071226] to-[#393b2c]',
+    },
+    {
+      title: 'a3_2_2',
+      desc: `Mint NFTs to gain privileges and support creators`,
+      className: 'bg-pink-200 rounded-[16px] w-1/3 p-12 flex items-center justify-center bg-gradient-to-r from-[#393b2c] to-[#3b3d27]',
+    },
+    {
+      title: 'a3_2_3',
+      desc: `Players can create puzzles and earn for their supporting the ecosystem`,
+      className: 'bg-pink-200 rounded-[16px] w-1/3 p-12 flex items-center justify-center bg-gradient-to-r from-[#393b2c] to-[#071226]',
+    },
+  ]
+  const a3_3 = `In future, Virtuoso Club can offer competitive tournaments for cash prizes. Live stream games and allow audience to engage. And much more.`;
+  const a3_3_extra = `Virtuoso Club is currently only deployed on Base Chain`;
+  
   return (
-    <main className="flex lg:flex-row md:flex-col-reverse flex-col-reverse justify-center items-center">
-      <div className="w-full lg:w-2/3 flex flex-col">
-        <div className="lg:flex flex-row space-x-5 items-center hidden ">
-          <Image src={logo} alt="logo" width={40} />
-          <p className="font-mono text-4xl">Virtuoso Club</p>
+    <main className="px-10">
+      <div className="flex lg:flex-row md:flex-col-reverse flex-col-reverse justify-center items-center py-24">
+        <div className="w-full lg:w-2/3 flex flex-col justify-center">
+          <FAQ />
         </div>
-        <FAQ />
+        <div className="w-full lg:w-1/3 mr-0 flex flex-col items-center ">
+          <iframe
+            className="md:w-[600px] md:h-[600px] w-[87vw] h-[87vw] rounded-lg overscroll-hidden"
+            src="https://www.fiveoutofnine.com/api/chess/asset/2"
+            height={600}
+            width={600}
+          />
+          <div className="mt-2 text-xs opacity-30">art credit: fiveoutofnine</div>
+        </div>
       </div>
-      <div className="w-full lg:w-1/3 mr-0 flex flex-col justify-center items-center">
-        <iframe
-          className="md:w-[500px] md:h-[500px] w-[87vw] h-[87vw]"
-          src="https://www.fiveoutofnine.com/api/chess/asset/2"
-          height={500}
-          width={500}
-        />
-        <div className="mt-2 text-xs opacity-30">art credit: fiveoutofnine</div>
+      <div className="text-center">
+        <h2 className={`mb-6 text-2xl pixeloid-sans-bold`}>{q3}</h2>
+        <div className={`pixeloid-sans`}>
+          <p className="mb-4">{a3_1}</p>
+          <p className="mb-8">{a3_2}</p>
+          <div className="mb-4 flex gap-x-5 justify-center pixeloid-sans-bold text-lg text-[#E6FA04]">
+            {cards.map(card => <div className={`${card.className}`}>{card.desc}</div>)}
+          </div>
+          <br/>
+          <p>{a3_3}</p>
+          <br/><br/><br/><br/>
+        </div>
       </div>
     </main>
   );
--- landing/components/FAQ.tsx
+++ landing/components/FAQ.tsx
@@ -14,43 +14,20 @@ the game. A recent example of this is Chess.com's controversial \
 decision of banning Hans Niemann and accusing him of cheating without \
 providing sufficient proof which created a stir among the chess community.`;
 
-  const q3 = `How does Virtuoso Club work?`;
-  const a3_1 = `We believe the key is to build incentives that reward Chess “creators” \
-for promoting and maintaining the game.`;
-  const a3_2 = `As of today, Virtuoso Club allows players to get better at the game of chess by solving puzzles of various difficulty:`;
-  const a3_2_1 = `a) Players compete amongst each other`;
-  const a3_2_2 = `b) Players mint NFTs to gain privileges`;
-  const a3_2_3 = `c) Players can create puzzles and earn ETH`;
-  const a3_3 = `In the future, we will allow players to play full games, offer competitive tournaments, and even allow the audience to engage.`;
-  const a3_3_extra = `Virtuoso Club is currently only deployed on Base Chain`;
-
   return (
-    <div className="flex flex-col lg:mt-10 lg:max-w-[700px]">
-      <div className="lg:mt-10 mt-5">
-        <h2 className={`mb-3 text-2xl font-semibold`}>{q1}</h2>
-        <div className={`max-w-[750px] text-sm opacity-70`}>
+    <div className="flex flex-col lg:max-w-[700px] pixeloid-sans">
+      <div className="">
+        <h2 className={`mb-4 text-2xl pixeloid-sans-bold`}>{q1}</h2>
+        <div className={`max-w-[750px]`}>
           <p>{a1}</p>
         </div>
       </div>
       <div className="lg:mt-10 mt-5">
-        <h2 className={`mb-3 text-2xl font-semibold`}>{q2}</h2>
-        <div className={`max-w-[750px] text-sm opacity-70`}>
+        <h2 className={`mb-4 text-2xl pixeloid-sans-bold`}>{q2}</h2>
+        <div className={`max-w-[750px]`}>
           <p>{a2_1}</p>
           <br />
           <p>{a2_2}</p>
-        </div>
-      </div>
-      <div className="lg:mt-10 mt-5">
-        <h2 className={`mb-3 text-2xl font-semibold`}>{q3}</h2>
-        <div className={`max-w-[750px] text-sm opacity-70`}>
-          <p>{a3_1}</p>
-          <br />
-          <p>{a3_2}</p>
-          <p>{a3_2_1}</p>
-          <p>{a3_2_2}</p>
-          <p>{a3_2_3}</p>
-          <br />
-          <p>{a3_3}</p>
         </div>
       </div>
     </div>
